4307830c15ab6f5e161dfcbfac8547035510d951
Yaneizy Castillo <yccastillo18@gmail.com>
2021-09-23 15:22:09 -0400

    Completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,27 @@ require 'pry'
 
 class String
 
+  #first method
   def sentence?
-    
+    self.end_with?(".")
   end
 
+
+  #second method
   def question?
-
+   self.end_with?("?")
   end
 
+
+  #third method
   def exclamation?
-
+   self.end_with?("!")
   end
 
+
+  #fourth method
   def count_sentences
-
+   self.split(/\.|\?|!/).filter{ |sentence| !sentence.empty? }.size
   end
+
 end
